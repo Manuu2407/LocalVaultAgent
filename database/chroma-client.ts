2b54419ff571c8dc4be9c8ab0e64e1f4477ce63b
Manuel Rocholz <manuel.rocholz@voss.net>
2025-06-03 18:28:34 +0200

    implemented function to store documents to vectore store

--- database/chroma-client.ts
+++ database/chroma-client.ts
@@ -1,8 +1,28 @@
 import { ChromaClient } from "chromadb";
-const client = new ChromaClient({path: "http://localhost:8000"});
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import type { Document } from "@langchain/core/documents";
+import "jsr:@std/dotenv/load";
 
-export async function getOrCreateCollection(name: string) {
-  return await client.getOrCreateCollection({
-    name
-  });
+const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY") || "";
+const LANG_SMITH_API_KEY = Deno.env.get("LANGSMITH_API_KEY") || "";
+const LANG_SMITH_TRACING = Deno.env.get("LANGSMITH_TRACING") || false;
+const EMBEDDINGS_MODEL = Deno.env.get("EMBEDDINGS_MODEL") || "";
+const CHROMA_DB_URL = Deno.env.get("CHROMA_DB_URL") || "";
+
+const embeddings = new OpenAIEmbeddings({
+  model: EMBEDDINGS_MODEL,
+  openAIApiKey: OPENAI_API_KEY,
+});
+
+const vectorStore = new Chroma(embeddings, {
+  collectionName: "local-documents",
+  url: CHROMA_DB_URL,
+  collectionMetadata: {
+    "hnsw:space": "cosine",
+  },
+});
+
+export async function addDocumentsToVectorStore(documents: Document<Record<string, any>>[]) {;
+  return await vectorStore.addDocuments(documents); 
 }
