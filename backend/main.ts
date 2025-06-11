6fda40d5a758524692e52a7a7bd71d697e893e5a
Manuel Rocholz <manuel.rocholz@voss.net>
2025-06-11 11:09:57 +0200

    added logic to test workflows

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,22 @@
 import { loadLocalDocuments } from "./file-loader/DirectoryLoader.ts";
-import { addDocumentsToVectorStore } from "./database/chroma-client.ts";
+import { addDocumentsToVectorStore, similaritySearch } from "./database/chroma-client.ts";
 
-const documents = await loadLocalDocuments();
 
-console.log(await addDocumentsToVectorStore(documents));
+// Set these variables to test the worflows
+const LOAD_AND_STORE = false;
+const SEARCH = true;
+
+const SEARCH_QUERY = "";
+const kNN = 2;
+
+if (LOAD_AND_STORE) {
+    const documents = await loadLocalDocuments();
+
+    const result = await addDocumentsToVectorStore(documents);
+    console.log("Added documents to vector store:", result);
+    }
+
+if (SEARCH) {
+    const searchResult = await similaritySearch(SEARCH_QUERY, kNN);
+    console.log("Search result:", searchResult);
+}
