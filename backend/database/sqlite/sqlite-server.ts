4b9e1bdaef2df226bc0a3102407bfb9e41520a8e
Manuel Rocholz <manuel.rocholz@voss.net>
2025-06-12 17:01:50 +0200

    moved SQLITE_PORT to .env

--- backend/database/sqlite/sqlite-server.ts
+++ backend/database/sqlite/sqlite-server.ts
@@ -2,6 +2,9 @@ import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import invoiceRouter from "./invoiceRouter.ts";
 import express from 'express';
+import "jsr:@std/dotenv/load";
+
+const SQLITE_PORT = Deno.env.get('SQLITE_PORT') || 3000;
 
 export const db = await open({
   filename: './database/sqlite/local-vault-agent.db',
@@ -12,7 +15,7 @@ const invoicesSchema = await Deno.readTextFile('./database/sqlite/invoices.sql')
 await db.exec(invoicesSchema);
 
 const app = express();
-const port = 3000;
+const port = SQLITE_PORT;
 
 app.locals.db = db;
 
