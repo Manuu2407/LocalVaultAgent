6440a0bc2be47e85b0809109fa694387c596bf24
Manuel Rocholz <manuel.rocholz@voss.net>
2025-06-17 17:04:16 +0200

    Implement invoice data extraction

---
+++ backend/LLMs/finance.ts
@@ -0,0 +1,49 @@
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOllama } from "@langchain/ollama";
+import { wrapSDK } from "langsmith/wrappers";
+import { extractionPromptTemplate } from "./prompts/templates.ts";
+import "jsr:@std/dotenv/load";
+
+const DATA_EXTRACTION_MODEL = Deno.env.get("DATA_EXTRACTION_MODEL") || "qwen3:8b";
+
+const llm = wrapSDK(new ChatOllama({
+  model: DATA_EXTRACTION_MODEL,
+  temperature: 0,
+  maxRetries: 2,
+  format: "json"
+}));
+
+async function extractData(template: PromptTemplate, input: Record<string, any>) {
+  const formattedPrompt = await template.format(input);
+  const response = await llm.invoke(formattedPrompt);
+  return response.content.toString();
+}
+
+export async function extractInvoiceData(text: string) {
+  const staticParams = {
+    type: "invoices",
+    schemaDescription: `
+        - invoice_no: The invoice number as a string (e.g., "INV-1234").
+        - issue_date: The date the invoice was issued, as a string in "YYYY-MM-DD" format.
+        - due_date: The due date for payment, as a string in "YYYY-MM-DD" format.
+        - currency: The currency code as a string (e.g., "EUR").
+        - total_amount: The total amount of the invoice as a number (e.g., 319.50).
+        - vendor: The name of the vendor or company issuing the invoice as a string.
+        `,
+    schema: JSON.stringify(
+     {
+        invoice_no: { type: "string" },
+        issue_date: { type: "string" },
+        due_date: { type: "string" },
+        total_amount: { type: "number" },
+        currency: { type: "string" },
+        vendor: { type: "string" }
+     }, null, 2),
+    additionalInstructions: `
+        - If no or multiple currencies are mentioned, prioritize or default to EUR.
+        - If total_amounts for different currencies are mentioned, choose the value that belongs to the chosen currency.
+        `,
+  };
+  const promptInput = { ...staticParams, text };
+  return await extractData(extractionPromptTemplate, promptInput);
+}
--- backend/main.ts
+++ backend/main.ts
@@ -1,10 +1,11 @@
 import { chunkDocuments, loadLocalDocuments } from "./file-loader/DirectoryLoader.ts";
 import { addDocumentsToVectorStore, similaritySearch } from "./database/chroma-client.ts";
-
+import { extractInvoiceData } from "./LLMs/finance.ts";
 
 // Set these variables to test the worflows
 const LOAD_AND_STORE = false;
-const SEARCH = true;
+const SEARCH = false;
+const FINANCE = true;
 
 // Similarity search parameters
 const SEARCH_QUERY = "";
@@ -16,14 +17,23 @@ const CHUNK_OVERLAP = 25;
 
 if (LOAD_AND_STORE) {
     const documents = await loadLocalDocuments();
-
+    
     const chunkedDocuments = await chunkDocuments(documents, CHUNK_SIZE, CHUNK_OVERLAP);
-
+    
     const result = await addDocumentsToVectorStore(chunkedDocuments);
     console.log("Added documents to vector store:", result);
-    }
+}
 
 if (SEARCH) {
     const searchResult = await similaritySearch(SEARCH_QUERY, kNN);
     console.log("Search result:", searchResult);
+}
+
+if (FINANCE) {
+    const documents = await loadLocalDocuments();
+
+    for (const doc of documents) {
+        const invoiceData = await extractInvoiceData(doc.pageContent);
+        console.log(JSON.parse(invoiceData));
+    }
 }
